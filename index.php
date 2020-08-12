661d4ca2fbd1f33283092c36a38496cfca4f0f54
FaroukDourmane <56443340+FaroukDourmane@users.noreply.github.com>
2020-08-12 11:55:50 +0300

    finish landing page v0.1

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php require_once("config/config.php"); ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -9,7 +8,58 @@
     <link rel="stylesheet" href="assets/css/main.css">
 
   </head>
+
   <body>
+    <div class="landing-container">
+      <div class="top">
+        <div class="left-side">
+          <div class="logo">
+            <img src="assets/svg/logo.svg" />
+          </div>
+
+          <ul>
+            <li> <a href="#"> <img src="assets/svg/home.svg" /> Home </a> </li>
+            <li> <a href="#"> <img src="assets/svg/hookah.svg" /> Products </a> </li>
+            <li> <a href="#"> <img src="assets/svg/question.svg" /> Who we are </a> </li>
+            <li> <a href="#"> <img src="assets/svg/contact.svg" /> Contact us </a> </li>
+          </ul>
+        </div>
+
+        <div class="right-side">
+          <div class="top-menu">
+            <ul class="list">
+              <li><a>+000 000 000 00</a></li>
+              <li><a href="#"> <img src="assets/svg/facebook.svg" /> </a></li>
+              <li><a href="#"> <img src="assets/svg/instagram.svg" /> </a></li>
+              <li><a href="#"> <img src="assets/svg/twitter.svg" /> </a></li>
+            </ul>
+
+            <ul class="lang">
+              <ul>
+                <li class="main">
+                  <a href="#" class="main">EN</a>
+                  <!-- <ul>
+                    <li> <a href="#"> TR </a> </li>
+                    <li> <a href="#"> AR </a> </li>
+                  </ul> -->
+                </li>
+              </ul>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="content">
+        <div class="slide-container">
+
+        </div>
+
+        <div class="text">
+          <h1>Smoke with legacy</h1>
+          <p> <b>NaraJammo</b>, we provide the best</p>
+        </div>
+      </div>
+    </div>
   </body>
 
   <footer>
