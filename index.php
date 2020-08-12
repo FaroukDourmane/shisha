e9be468d1f26359fbc9ce3d5bc961b6cd4a9cdcf
FaroukDourmane <56443340+FaroukDourmane@users.noreply.github.com>
2020-08-12 11:02:07 +0300

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php require_once("config/config.php"); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Nara Jammo - hookah store</title>
+    <link rel="icon" href="assets/img/boat.ico"/>
+    <link rel="stylesheet" href="assets/css/main.css">
+
+  </head>
+  <body>
+  </body>
+
+  <footer>
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+  </footer>
+</html>
